eb237365bc353f1ed7e701e6c19d9aa37691775a
mr-norbert <bnobertprivate@gmail.com>
2022-03-15 03:06:12 -0700

    Initial commit

---
+++ src/main/java/bnorbert/springbootaerial/SpringBootAerialApplication.java
@@ -0,0 +1,13 @@
+package bnorbert.springbootaerial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAerialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAerialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bnorbert/springbootaerial/SpringBootAerialApplicationTests.java
@@ -0,0 +1,13 @@
+package bnorbert.springbootaerial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAerialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
